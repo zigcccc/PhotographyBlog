777a2f8e2709eb97ca88e16f94654d6e99a36b5a
Žiga Krašovec <zkrasovec@gmail.com>
2018-06-21 08:54:11 +0200

    Added GoogleAnalytics functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue/dist/vue';
 import VueRouter from 'vue-router';
+import VueAnalytics from 'vue-analytics';
 import App from './App.vue';
 import router from '@/router';
 import store from '@/store';
@@ -10,6 +11,11 @@ import MyButton from '@/components/MyButton';
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+Vue.use(VueAnalytics, {
+	id: 'UA-109301417-2',
+	checkDuplicatedScript: true,
+	router
+});
 
 Vue.component('PageContainer', PageContainer);
 Vue.component('MyButton', MyButton);
